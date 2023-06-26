f66219d3971270dedc835cfde164861a56b12fb3
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-06-26 18:44:36 +0300

    ES6 fixed lint error

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-	const budget = getBudgetObject(income, gdp, capita);
-	return {
-		...budget,
-		getIncomeInDollars(income) {
-			return `$${income}`;
-		},
-		getIncomeInEuros(income) {
-			return `${income} euros`;
-		},
-	}
+  const budget = getBudgetObject(income, gdp, capita);
+  return {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
 }
