6146bbab1ad22ce2eb91bbc620b44aa878b56eaa
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-06-29 13:08:45 +0300

    primitive class

---
+++ 0x02-ES6_classes/8-main.js
@@ -0,0 +1,6 @@
+import { string } from "yargs";
+import HolbertonClass from "./8-hbtn_class.js";
+
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(Number(hc));
+console.log(String(hc));
