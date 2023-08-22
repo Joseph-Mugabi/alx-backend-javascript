aca0437a9398a70efe1cc07ddce1daa4ca604668
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-08-22 20:44:08 +0300

    http req

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,13 +4,13 @@ const http = require("http");
 const port = 1245
 
 const app = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/plain");
-    res.statusCode = 200
-    res.end("Hello Holberton School!");
+  res.setHeader("Content-Type", "text/plain");
+  res.statusCode = 200
+  res.end("Hello Holberton School!");
 })
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 
 module.exports = app;
